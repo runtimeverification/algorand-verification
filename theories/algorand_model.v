c3649cd158c04a702c3fee3ac6b19df52c21a210
Lucas Pena <lpena7@illinois.edu>
2019-05-10 15:36:51 -0500

    proved nextvote_val_precondition

--- theories/algorand_model.v
+++ theories/algorand_model.v
@@ -3382,7 +3382,31 @@ Lemma nextvote_val_precondition g1 g2 uid v r p s:
   forall u, g1.(users).[?uid] = Some u ->
   (exists b, nextvote_val_ok u uid v b r p s) \/
   (exists b v', nextvote_stv_ok u uid v' b r p s) .
-Admitted.
+Proof.
+  move => H_sent u H_u.
+  destruct H_sent as [ms [H_msg [[d1 [in1 H_step]]|H_step]]].
+  * { (* message delivery cases *)
+      destruct H_step as (key_ustate & ustate_post & H_step & H_honest
+                          & key_mailbox & H_msg_in_mailbox & ->).
+      remember (ustate_post,ms) as ustep_out in H_step.
+      destruct H_step; injection Hequstep_out; clear Hequstep_out;
+      intros <- <-; revert H_msg; move/Iter.In_mem => H_msg; try contradiction.
+      simpl in H_msg; destruct H_msg; try contradiction; inversion H0.
+    }
+  * { (* internal transition cases *)
+      destruct H_step as (key_user & ustate_post & H_honest & H_step & ->).
+      remember (ustate_post,ms) as ustep_out in H_step.
+      assert ((global_state.users UserId UState [choiceType of Msg] g1) [` key_user] = u).
+      rewrite in_fnd in H_u; inversion H_u; trivial.
+      rewrite H in H_step. clear H.
+      destruct H_step; injection Hequstep_out; clear Hequstep_out;
+        intros <- <-; revert H_msg; move/Iter.In_mem => H_msg; try contradiction;
+      simpl in H_msg; destruct H_msg as [H_msg | H_msg]; try contradiction;
+      try destruct H_msg as [H_msg | H_msg]; inversion H_msg.
+      left; exists b; subst; assumption.
+      right; exists b; exists v0; subst; apply H.
+    }
+Qed.
 
 Lemma softvotes_utransition_internal:
   forall uid pre post msgs, uid # pre ~> (post, msgs) ->
