9bc90e056a4dfa08308d503e9d558626eee6610b
Brandon Moore <brandon.moore@runtimeverification.com>
2019-05-11 02:25:26 -0500

    Prove certvote_precondition

--- theories/algorand_model.v
+++ theories/algorand_model.v
@@ -3346,7 +3346,35 @@ Lemma certvote_postcondition uid v r p g1 g2:
   v \in certvals u r p /\
         exists b, b \in u.(blocks) r /\ valid_block_and_hash b v.
 Proof using.
-Admitted.
+  move => H_sent u H_u.
+  destruct H_sent as [ms [H_msg [[d1 [in1 H_step]]|H_step]]].
+  (* need to grab u *)
+  * { (* message delivery cases *)
+      destruct H_step as (key_ustate & ustate_post & H_step & H_honest
+                          & key_mailbox & H_msg_in_mailbox & ->).
+      remember (ustate_post,ms) as ustep_out in H_step.
+      destruct H_step; injection Hequstep_out; clear Hequstep_out;
+      intros <- <-; revert H_msg; move/Iter.In_mem => H_msg; try contradiction.
+      simpl in H_msg; destruct H_msg; try contradiction.
+      inversion H0;subst;clear H0.
+      unfold certvote_ok in H;decompose record H;clear H.
+      rewrite fnd_set eq_refl in H_u. case: H_u => {u}<-.
+      unfold certvote_result.
+      split;[|exists b;split];assumption.
+    }
+  * { (* internal transition cases *)
+      destruct H_step as (key_user & ustate_post & H_honest & H_step & ->).
+      remember (ustate_post,ms) as ustep_out in H_step.
+      destruct H_step; injection Hequstep_out; clear Hequstep_out;
+        intros <- <-; move/Iter.In_mem in H_msg; try contradiction;
+      simpl in H_msg; destruct H_msg as [H_msg | H_msg]; try contradiction;
+      try destruct H_msg as [H_msg | H_msg]; inversion H_msg;subst;clear H_msg.
+      rewrite fnd_set eq_refl in H_u. case:H_u => H_u.
+      subst.
+      unfold certvote_ok in H;decompose record H;clear H.
+      split;[|exists b;split];assumption.
+    }
+Qed.
 
 Lemma nextvote_open_precondition g1 g2 uid r p s:
   user_sent uid (Nextvote_Open, step_val s, r, p, uid) g1 g2 ->
